a224b82c85efd9e1be16fb31ee3418e67b76d9b5
Reascer <48482533+Reascer@users.noreply.github.com>
2023-11-10 00:16:36 +0100

    Revert "voodoo mamba"
    This reverts commit 7c09ab824d357e1405a26c1215446040fad1c60c.

--- src/main/java/reascer/wom/skill/dodges/EnderStepSkill.java
+++ src/main/java/reascer/wom/skill/dodges/EnderStepSkill.java
@@ -1,38 +1,21 @@
 package reascer.wom.skill.dodges;
 
-import java.util.UUID;
-
 import io.netty.buffer.Unpooled;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import reascer.wom.gameasset.WOMSkills;
 import yesman.epicfight.client.events.engine.ControllEngine;
 import yesman.epicfight.client.world.capabilites.entitypatch.player.LocalPlayerPatch;
-import yesman.epicfight.network.client.CPExecuteSkill;
+import yesman.epicfight.gameasset.EpicFightSkills;
 import yesman.epicfight.skill.Skill;
-import yesman.epicfight.skill.SkillContainer;
 import yesman.epicfight.skill.dodge.DodgeSkill;
+import yesman.epicfight.skill.dodge.StepSkill;
 import yesman.epicfight.world.capabilities.entitypatch.player.ServerPlayerPatch;
-import yesman.epicfight.world.entity.eventlistener.PlayerEventListener.EventType;
-import yesman.epicfight.world.entity.eventlistener.SkillConsumeEvent;
 
-public class EnderStepSkill extends DodgeSkill {
-	private static final UUID EVENT_UUID = UUID.fromString("38cb04e1-9751-445f-82bd-fb61426a58c7");
-	
+public class EnderStepSkill extends StepSkill {
 	public EnderStepSkill(DodgeSkill.Builder builder) {
 		super(builder);
-	}
-	
-	@Override
-	public void onInitiate(SkillContainer container) {
-		super.onInitiate(container);
-	}
-	
-	@Override
-	public void onRemoved(SkillContainer container) {
-		super.onRemoved(container);
 	}
 	
 	@OnlyIn(Dist.CLIENT)
@@ -52,54 +35,13 @@ public class EnderStepSkill extends DodgeSkill {
 		return buf;
 	}
 	
-	@OnlyIn(Dist.CLIENT)
-	@Override
-	public Object getExecutionPacket(LocalPlayerPatch executer, FriendlyByteBuf args) {
-		int forward = args.readInt();
-		int backward = args.readInt();
-		int left = args.readInt();
-		int right = args.readInt();
-		int vertic = forward + backward;
-		int horizon = left + right;
-		int degree = vertic == 0 ? 0 : -(90 * horizon * (1 - Math.abs(vertic)) + 45 * vertic * horizon);
-		int animation;
-		
-		if (vertic == 0) {
-			if (horizon == 0) {
-				animation = 4;
-			} else {
-				animation = horizon >= 0 ? 2 : 3;
-			}
-		} else {
-			animation = vertic >= 0 ? 0 : 1;
-		}
-		
-		CPExecuteSkill packet = new CPExecuteSkill(executer.getSkill(this).getSlotId());
-		packet.getBuffer().writeInt(animation);
-		packet.getBuffer().writeFloat(degree);
-		
-		return packet;
-	}
-	
 	@Override
 	public void executeOnServer(ServerPlayerPatch executer, FriendlyByteBuf args) {
-		SkillConsumeEvent event = new SkillConsumeEvent(executer, this, this.resource, true);
-		executer.getEventListener().triggerEvents(EventType.SKILL_CONSUME_EVENT, event);
-		
-		if (!event.isCanceled()) {
-			event.getResourceType().consumer.consume(this, executer, event.getAmount());
-		}
-		
-		executer.getSkill(this).activate();
-		int i = args.readInt();
-		float yaw = args.readFloat();
-		
-		executer.playAnimationSynchronized(this.animations[i], 0);
-		executer.changeModelYRot(yaw);
+		super.executeOnServer(executer, args);
 	}
 	
 	@Override
 	public Skill getPriorSkill() {
-		return WOMSkills.ENDERSTEP;
+		return EpicFightSkills.STEP;
 	}
 }
